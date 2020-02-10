5fa9aca01abd309f1d09ac76b54dca579892d0b2
Jakob Borg <borg.jakob@gmail.com>
2020-02-10 16:23:23 +0100

    integrating to x_end = 3

--- Numerical_projects/CXX_Template/Utils.h
+++ Numerical_projects/CXX_Template/Utils.h
@@ -49,7 +49,7 @@ extern struct ConstantsAndUnits {
   
   // Min and max x-value
   const double x_start = - 25;  //log(1e-8);
-  const double x_end   = 0.0;
+  const double x_end   = 3.0;
 
   // Include polarization and/or neutrinos?
   const bool polarization  = true;
