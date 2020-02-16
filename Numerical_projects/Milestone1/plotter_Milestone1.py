ac1fa123f24069cc50b9e007bab0204adbe26830
Jakob Borg <borg.jakob@gmail.com>
2020-02-16 18:56:48 +0100

    plotter milestone1 done

--- Numerical_projects/Milestone1/plotter_Milestone1.py
+++ Numerical_projects/Milestone1/plotter_Milestone1.py
@@ -14,10 +14,13 @@ x_array, eta_of_x, H_of_x, dHdx_of_x, Hp_of_x, dHpdx_of_x, ddHpddx_of_x, OmegaB,
 
 # Data handling and converting
 # ----------------------------
-x_array_ticks = np.linspace(x_array.min(), x_array.max(), 6)
 z_array = 1/np.exp(x_array) - 1
 z_non_neg = z_array[z_array > 0]
 a_array = np.exp(x_array)
+x_array_ticks = np.linspace(x_array.min(), x_array.max(), 5)
+z_array_ticks = np.logspace(np.log10(z_non_neg.min()), np.log10(z_non_neg.max()), 5)
+z_array_ticks = np.logspace(7,-2,4)
+
 km = 1e3                                # [m]
 Mpc = 3.08567758e22                     # [m]
 h = 0.7                                 # unitless hubble
@@ -36,7 +39,8 @@ aeta_R = c/Hp_of_x/Mpc/km                      # analytical eta in radiation dom
 idx_astar = np.argmax(Omega_matter)
 aeta_M = (eta_of_x[idx_astar] + 2*c*(1/Hp_of_x-1/Hp_of_x[idx_astar]))/Mpc/km
 idx_alamda = np.argmax(OmegaLambda)
-aeta_Lambda = (eta_of_x[idx_alamda] - 3*c*(1/(np.exp(x_array*2)**2*Hp_of_x)-1/(np.exp(x_array*2)[idx_alamda]**2*Hp_of_x[idx_alamda])))/Mpc/km
+aeta_Lambda = (eta_of_x[idx_alamda] - 3*c*(1/(a_array**2*Hp_of_x) -
+                                           1/(a_array[idx_alamda]**2*Hp_of_x[idx_alamda])))/Mpc/km
 
 # Plotting
 # --------
@@ -50,6 +54,7 @@ ax.plot(x_array, Omega_matter, label=r'$\Omega_{\rm{Matter}}$', ls='--', alpha=0
 ax.axvspan(x_array.min(), x_array[idx_rad_mat_eq], alpha=0.25, color='C0')
 ax.axvspan(x_array[idx_rad_mat_eq], x_array[idx_mat_lambda_eq], alpha=0.25, color='C1')
 ax.axvspan(x_array[idx_mat_lambda_eq], x_array.max(), alpha=0.25, color='C3')
+ax.margins(x=0)
 ax.legend(loc=6)
 ax.set_xlabel(r'$x = ln(a)$')
 ax.set_ylabel(r'$\Omega_i = \frac{\rho_i}{\rho_c}$')
@@ -61,36 +66,36 @@ fig.savefig('./figs/omegas_of_x.pdf', bbox_inces='tight')
 fig, axes = plt.subplots(2, 2)
 axes[0, 0].plot(x_array, H_units, color='C2')
 axes[0, 0].set_ylabel(r'$H(x) \left[\rm{km}\,\rm{s^{-1}}\,\rm{Mpc^{-1}}\right]$')
-axes[0, 0].set_xticks(x_array_ticks)
 
 # Plotting Hubble parameter vs z
 axes[0, 1].plot(z_non_neg, H_units[z_array > 0], color='C2')
 axes[0, 1].set_xlabel(r'$z = a^{-1} - 1$')
 axes[0, 1].set_ylabel(r'$H(z) \left[\rm{km}\,\rm{s^{-1}}\,\rm{Mpc^{-1}}\right]$')
-axes[0, 1].set_xlim(z_non_neg.max()+z_non_neg.max()*3, z_non_neg.min()+0.0001)
+axes[0, 1].set_xlim(z_non_neg.max(), z_non_neg.min()+1e-8) #+z_non_neg.max()*3
 axes[0, 1].set_xscale('log')
+axes[0, 1].set_xticks(z_array_ticks)
 
 # Plotting conformal time
 axes[1, 0].plot(x_array, eta_units, color='C2')
-axes[1, 0].plot(x_array, aeta_R, color='C0', ls = '-.')
-axes[1, 0].plot(x_array, aeta_M, color='C1', ls = '-.')
-axes[1, 0].plot(x_array, aeta_Lambda, color='C3', ls = '-.')
+axes[1, 0].plot(x_array, aeta_R, color='C0', ls='-.')
+axes[1, 0].plot(x_array, aeta_M, color='C1', ls='-.')
+axes[1, 0].plot(x_array, aeta_Lambda, color='C3', ls='-.')
 axes[1, 0].set_ylabel(r'$\eta(x) [\rm{Gpc}]$')
-#axes[1, 0].set_xticks(x_array_ticks)
 
 # PLotting H-prime
 axes[1, 1].plot(x_array, Hp_units, color='C2')
 axes[1, 1].set_ylabel(r'$\mathcal{H}(x) \left[\rm{km}\,\rm{s^{-1}}\,\rm{Mpc^{-1}}\right]$')
-axes[1, 1].set_xticks(x_array_ticks)
 
 # Filling background in each axis
 for i, ax in enumerate(axes.flatten()):
     ax.set_yscale('log')
+    ax.margins(x=0)
     if i != 1:
         ax.axvspan(x_array.min(), x_array[idx_rad_mat_eq], alpha=0.25, color='C0')
         ax.axvspan(x_array[idx_rad_mat_eq], x_array[idx_mat_lambda_eq], alpha=0.25, color='C1')
         ax.axvspan(x_array[idx_mat_lambda_eq], x_array.max(), alpha=0.25, color='C3')
         ax.set_xlabel(r'$x = ln(a)$')
+        ax.set_xticks(x_array_ticks)
     else:
         ax.axvspan(z_array.max(), z_array[idx_rad_mat_eq], alpha=0.25, color='C0')
         ax.axvspan(z_array[idx_rad_mat_eq], z_array[idx_mat_lambda_eq], alpha=0.25, color='C1')
