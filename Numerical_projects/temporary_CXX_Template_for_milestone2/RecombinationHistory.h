8b2b4aa8c39fa9738e6d17a53d6ee94887e97eba
Jakob Borg <borg.jakob@gmail.com>
2020-03-04 16:15:46 +0100

    tried changing x-interval and nrpoints for smoother tau solution. failed

--- Numerical_projects/temporary_CXX_Template_for_milestone2/RecombinationHistory.h
+++ Numerical_projects/temporary_CXX_Template_for_milestone2/RecombinationHistory.h
@@ -23,7 +23,7 @@ class RecombinationHistory{
     const double x_end    = Constants.x_end;
     
     // Numbers of points of Xe,ne array (modify as you see fit)
-    const int npts_rec_arrays = 1e+4;//10000;
+    const int npts_rec_arrays = 1e+5;//10000;
   
     // Xe for when to switch between Saha and Peebles
     const double Xe_saha_limit = 0.99;
