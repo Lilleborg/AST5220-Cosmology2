0d2a286911a27fb91c389a3ae12bfc28f43de64f
Jakob Borg <borg.jakob@gmail.com>
2020-03-08 15:34:40 +0100

    even more tweaking... plotting soon done

--- Numerical_projects/Milestone2/plotter_milestone2.py
+++ Numerical_projects/Milestone2/plotter_milestone2.py
@@ -5,23 +5,26 @@ import sys
 sys.path.append('./../Milestone1/')
 from plotter_Milestone1 import color_each_regime
 
-# mpl.rcdefaults()
 # Plot styling
 plt.style.use('bmh')
 mpl.rc('font', family='serif', size=15)
 mpl.rc('text', usetex=False)
-figsize_single = (6,4.5)
 
 # Load data
 x_array, Xe, ne, tau, tau_deriv, tau_2deriv, g, g_deriv, g_2deriv =\
      np.loadtxt('../data/recombination.txt', unpack=True)
 
 # Data handling and some numerical testing of g_tilde
-# xstar and xrec taken from print from cmb
+# xstar and xrec taken from print from ./cmb
 xstar = -6.98608
 xrec  = -7.1649
+zoomed_xlim = [xstar+xstar*0.1, xstar-xstar*0.1]
+print(zoomed_xlim)
 
 x_array_ticks = np.append(np.linspace(x_array.min(), x_array.max(), 5),0)
+x_zoomed_ticks = zoomed_xlim
+x_zoomed_ticks.extend([xrec,xstar])
+print(x_zoomed_ticks)
 integrated_g_tilde = np.trapz(g,x_array)
 print('\nDifference between integrated g_tilde and 1:\nlog10(abs(integrated_g - 1)) =',\
      np.log10(np.abs(integrated_g_tilde-1)))
@@ -45,6 +48,7 @@ all_axes.extend(Xeax)
 for i in range(2):
     Xeax[i].semilogy(x_array,Xe,label='$Xe$')
     Xeax[i].tick_params(axis='y',labelcolor='C0')
+    Xeax[i].axhline(y=0.5,label=r'$Xe=0.5$',linestyle=(0,(5,10)),color='C6')
     
     # ne(x) overplotted
     neax = Xeax[i].twinx()
@@ -53,8 +57,12 @@ for i in range(2):
 
     if i == 0:
         Xeax[i].set_ylabel(r'$Xe = \frac{n_e}{n_b}$',color='C0')
+        Xeax[i].set_xticks(x_array_ticks)
+        Xeax[i].set_xticklabels(x_array_ticks,rotation=10)
     else:
         neax.set_ylabel(r'$n_e \, [\rm{1/m^{-3}}] $',color='C1')
+        Xeax[i].set_xticks(x_zoomed_ticks)
+        Xeax[i].set_xticklabels(['{:.2f}'.format(i) for i in x_zoomed_ticks],rotation=10)
 
 # tau(x)
 taufig, tauaxes = plt.subplots(1,2,figsize=(12,4.5))
@@ -69,6 +77,14 @@ for i in range(2):
     g_tau_plot = g_tau_ax.plot(x_array,g,color='C3',alpha=0.7,linewidth=1)
     g_tau_ax.tick_params(axis='y',labelcolor='C3')
 
+    if i == 0:
+        tauaxes[i].set_xticks(x_array_ticks)
+        tauaxes[i].set_xticklabels(x_array_ticks,rotation=10)
+    else:
+        g_tau_ax.set_ylabel(r'$\tilde{g}$',color='C3')
+        tauaxes[i].set_xticks(x_zoomed_ticks)
+        tauaxes[i].set_xticklabels(['{:.2f}'.format(i) for i in x_zoomed_ticks],rotation=10)
+
 # g_tilde(x)
 gfig, gax = plt.subplots(1,2,figsize=(12,4.5))
 all_axes.extend(gax)
@@ -76,33 +92,39 @@ for i in range(2):
     gax[i].plot(x_array,g/scalefactor_g,alpha=0.7,label=r'$\overline{\tilde{g}}$')
     gax[i].plot(x_array,g_deriv/scalefactor_g_deriv,alpha=0.7,label=r'$\overline{\tilde{g}^{\prime}}$')
     gax[i].plot(x_array,g_2deriv/scalefactor_g_2deriv,alpha=0.7,label=r'$\overline{\tilde{g}^{\prime\prime}}$')
+    
+    if i == 0:
+        gax[i].set_xticks(x_array_ticks)
+        gax[i].set_xticklabels(x_array_ticks,rotation=10)
+    else:
+        gax[i].set_xticks(x_zoomed_ticks)
+        gax[i].set_xticklabels(['{:.2f}'.format(i) for i in x_zoomed_ticks],rotation=10)
 
 # Some common handling of all axes
 for i,ax in enumerate(all_axes):
     color_each_regime(ax,x_array)
-    ax.set_xticks(x_array_ticks)
-    ax.axvline(x=xstar,linestyle='--',color='k',label=r'$x_{*}$',alpha=0.7,linewidth=1)
-    ax.axvline(x=xrec,linestyle='-.',color='k',label=r'$x_{rec}$',alpha=0.7,linewidth=1)
+    # ax.set_xticks(x_array_ticks)
+    ax.axvline(x=xstar,linestyle=(0,(5,10)),color='k',label=r'$x_{*}$',alpha=0.7,linewidth=1)
+    ax.axvline(x=xrec,linestyle=(0,(5,10,1,10)),color='r',label=r'$x_{rec}$',alpha=0.7,linewidth=1)
     ax.margins(x=0)
 
 # Final tweaking and saving
 # Xe and ne
-Xeax[1].set_xlim(xstar+xstar*0.1, xstar-xstar*0.1)
+Xeax[1].set_xlim(*zoomed_xlim)
 handles, labels = Xeax[0].get_legend_handles_labels()
 Xelegend = Xefig.legend(handles+neplot,labels+['\n'+'$n_e$'],bbox_to_anchor=(1.0,0.5),loc='center left')
 Xetitle = Xefig.suptitle('Fraction of free electrons')
 Xefig.savefig('./figs/free_electrons.pdf',bbox_extra_artists=(Xelegend,Xetitle),bbox_inches='tight')
 
-
 # Tau
-tauaxes[1].set_xlim(xstar+xstar*0.1, xstar-xstar*0.1)
+tauaxes[1].set_xlim(*zoomed_xlim)
 handles, labels = tauaxes[0].get_legend_handles_labels()
 taulegend = taufig.legend(handles+g_tau_plot,labels+['\n'+r'$\tilde{g}$'],bbox_to_anchor=(1.0,0.5),loc='center left')
 tautitle = taufig.suptitle('Optical depth and its derivatives')
 taufig.savefig('./figs/optical_depth.pdf',bbox_extra_artists=(taulegend,tautitle),bbox_inches='tight')
 
 # g_tilde
-gax[1].set_xlim(xstar+xstar*0.1, xstar-xstar*0.1)
+gax[1].set_xlim(*zoomed_xlim)
 handles, labels = gax[0].get_legend_handles_labels()
 glegend = gfig.legend(handles,labels,bbox_to_anchor=(1.0,0.5),loc='center left')
 gtitle = gfig.suptitle('Scaled visibility function and its derivatives')
