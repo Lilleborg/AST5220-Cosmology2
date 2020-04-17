48f8d44f26c53ec5b2950855a0f76773dfeda3eb
Jakob Borg <jakob.borg@astro.uio.com>
2020-04-17 19:31:37 +0200

    commented out some get functions i dont use

--- Numerical_projects/CXX_Template/BackgroundCosmology.h
+++ Numerical_projects/CXX_Template/BackgroundCosmology.h
@@ -68,12 +68,12 @@ class BackgroundCosmology{
     double get_OmegaB(double x = 0.0) const; 
     double get_OmegaM(double x = 0.0) const; 
     double get_OmegaR(double x = 0.0) const;
-    double get_OmegaRtot(double x = 0.0) const; 
+    // double get_OmegaRtot(double x = 0.0) const; 
     double get_OmegaNu(double x = 0.0) const;
     double get_OmegaCDM(double x = 0.0) const; 
     double get_OmegaLambda(double x = 0.0) const; 
     double get_OmegaK(double x = 0.0) const; 
-    double get_OmegaMnu(double x = 0.0) const; 
+    // double get_OmegaMnu(double x = 0.0) const; 
     double get_rho_crit(double x = 0.0) const;
     double get_H0() const;
     double get_h() const;
