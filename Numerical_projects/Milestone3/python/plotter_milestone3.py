425f1322d4bf0858eb5351e8122341656b43fe15
Jakob Borg <jakob.borg@astro.uio.com>
2020-04-22 20:09:05 +0200

    continue working on plotter

--- Numerical_projects/Milestone3/python/plotter_milestone3.py
+++ Numerical_projects/Milestone3/python/plotter_milestone3.py
@@ -11,7 +11,7 @@ mpl.rc('font', family='serif', size=15)
 mpl.rc('text', usetex=False)
 
 # Load data
-k_values = np.loadtxt("../../data/perturbations_k_values.txt",skiprows=1,usecols=0,delimiter="|")
+k_values, _, horizon_entry = np.loadtxt("../../data/perturbations_k_values.txt",skiprows=1,delimiter="|",unpack=True)
 quantities = ["x_array", "delta_cdm", "delta_b", "v_cdm", "v_b", "Phi", "Psi",\
     "Pi","Theta0", "Theta1", "Theta2","Source_T"]
 data = {key: [] for key in quantities}
@@ -22,53 +22,73 @@ for k_value in k_values:
 
     
 x_array = data["x_array"][0]
+x_array_ticks = np.append(np.linspace(x_array.min(), x_array.max(), 6), 0)
+x_array_ticks = np.array(x_array_ticks,dtype=int)
 all_axes = []
 
-delta_fig, delta_ax = plt.subplots()
-delta_fig.suptitle(r'$|\delta_{\rm{cdm}}|$, $|\delta_{\rm{b}}|$ in dashed')
+# Matter perturbations
+matter_fig, matter_axes = plt.subplots(2,1,sharex=True,figsize=(6,9))
+delta_ax, v_ax = matter_axes
 for ik,k in enumerate(k_values):
     delta_ax.plot(x_array,np.abs(data["delta_cdm"][ik]),color='C{:0d}'.format(ik),label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
-    delta_ax.plot(x_array,np.abs(data["delta_b"][ik]),'--',color='C{:0d}'.format(ik))#,label=r'$\delta_{\rm{b}}$, '+'$k = {:.5f}$'.format(k)+r'$/\rm{Mpc}$')
-delta_ax.set_yscale('log')
-all_axes.append(delta_ax)
+    delta_ax.plot(x_array,np.abs(data["delta_b"][ik]),'--',color='C{:0d}'.format(ik))
 
-potential_fig,potential_ax = plt.subplots()
-potential_fig.suptitle(r'$\Phi$, $\Psi$ in dashed')
-for ik,k in enumerate(k_values):
-    potential_ax.plot(x_array,data["Phi"][ik],color='C{:0d}'.format(ik),label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
-    potential_ax.plot(x_array,data["Psi"][ik],'--',color='C{:0d}'.format(ik))#,label=r'$\Psi$, '+'$k = {:.5f}$'.format(k)+r'$/\rm{Mpc}$')
-all_axes.append(potential_ax)
-
-v_fig, v_ax = plt.subplots()
-v_fig.suptitle(r'$|v_{\rm{cdm}}|$, $|v_{\rm{b}}|$ in dashed')
-for ik,k in enumerate(k_values):
     v_ax.plot(x_array,np.abs(data["v_cdm"][ik]),color='C{:0d}'.format(ik),label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
-    v_ax.plot(x_array,np.abs(data["v_b"][ik]),'--',color='C{:0d}'.format(ik))#,label=r'$v_{\rm{cdm}}$, '+'$k = {:.5f}$'.format(k)+r'$/\rm{Mpc}$')
-v_ax.set_yscale('log')
-all_axes.append(v_ax)
+    v_ax.plot(x_array,np.abs(data["v_b"][ik]),'--',color='C{:0d}'.format(ik))
 
-theta0_fig, theta0_ax = plt.subplots()
-theta0_fig.suptitle(r'$\delta_{\gamma} = 4\theta_0$')
+handles, labels = delta_ax.get_legend_handles_labels()
+matter_legend = matter_fig.legend(handles,labels,bbox_to_anchor=(1.0, 0.5), loc='center left')
+matter_title = matter_fig.suptitle("Matter perturbations")
+delta_ax.set_title(r'$|\delta_{\rm{cdm}}|$, $|\delta_{\rm{b}}|$ in dashed')
+delta_ax.set_yscale('log')
+v_ax.set_title(r'$|v_{\rm{cdm}}|$, $|v_{\rm{b}}|$ in dashed')
+v_ax.set_yscale('log')
+v_ax.set_xlabel(r'$x=\ln(a)$')
+all_axes.extend(matter_axes)
+
+# Multipoles
+theta_fig, theta_axes = plt.subplots(2,1,sharex=True,figsize=(6,9))
+theta0_ax,theta1_ax = theta_axes
 for ik,k in enumerate(k_values):
     theta0_ax.plot(x_array,4*data["Theta0"][ik],label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
-all_axes.append(theta0_ax)
+    theta1_ax.plot(x_array,-3*data["Theta1"][ik])#,label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
 
-theta1_fig, theta1_ax = plt.subplots()
-theta1_fig.suptitle(r'$v_{\gamma} = -3\theta_1$')
-for ik,k in enumerate(k_values):
-    theta1_ax.plot(x_array,-3*data["Theta1"][ik],label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
-all_axes.append(theta1_ax)
+handles, labels = theta0_ax.get_legend_handles_labels()
+theta_legend = theta_fig.legend(handles,labels,bbox_to_anchor=(1.0, 0.5), loc='center left')
+theta_title = theta_fig.suptitle("Multipole perturbations, "+r"$\ell = 0,\,\ell=1$")
+theta0_ax.set_title(r'$\delta_{\gamma} = 4\theta_0$')
+theta1_ax.set_title(r'$v_{\gamma} = -3\theta_1$')
+all_axes.extend(theta_axes)
+# potential_fig,potential_ax = plt.subplots()
+# potential_fig.suptitle(r'$\Phi$, $\Psi$ in dashed')
+# for ik,k in enumerate(k_values):
+#     potential_ax.plot(x_array,data["Phi"][ik],color='C{:0d}'.format(ik),label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
+#     potential_ax.plot(x_array,data["Psi"][ik],'--',color='C{:0d}'.format(ik))
+# all_axes.append(potential_ax)
 
-SourceT_fig, SourceT_ax = plt.subplots()
-SourceT_fig.suptitle(r'Temperature Source function')
-for ik,k in enumerate(k_values):
-    SourceT_ax.plot(x_array,data["Source_T"][ik],label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
-all_axes.append(SourceT_ax)
+# theta0_fig, theta0_ax = plt.subplots()
+# theta0_fig.suptitle(r'$\delta_{\gamma} = 4\theta_0$')
+# for ik,k in enumerate(k_values):
+#     theta0_ax.plot(x_array,4*data["Theta0"][ik],label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
+# all_axes.append(theta0_ax)
+
+# theta1_fig, theta1_ax = plt.subplots()
+# theta1_fig.suptitle(r'$v_{\gamma} = -3\theta_1$')
+# for ik,k in enumerate(k_values):
+#     theta1_ax.plot(x_array,-3*data["Theta1"][ik],label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
+# all_axes.append(theta1_ax)
+
+# SourceT_fig, SourceT_ax = plt.subplots()
+# SourceT_fig.suptitle(r'Temperature Source function')
+# for ik,k in enumerate(k_values):
+#     SourceT_ax.plot(x_array,data["Source_T"][ik],label=r'$k = {:g}$'.format(k)+r'$/\rm{Mpc}$')
+# all_axes.append(SourceT_ax)
 
 for ax in all_axes:
     color_each_regime(ax,x_array)
-    # ax.margins(x=0)
-    ax.legend()
-    ax.set_xlabel(r'$x=\ln(a)$')
-
+    ax.margins(x=0)
+    # ax.legend()
+    ax.set_xticks(x_array_ticks)
+    for ik in range(len(k_values)):
+        ax.axvline(x=horizon_entry[ik],linestyle=':',color='C{:0d}'.format(ik))
 plt.show()
