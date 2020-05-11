30d2c28ac99d0d442222029937658072a14b4f5b
Jakob Borg <jakob.borg@astro.uio.com>
2020-05-11 17:52:30 +0200

    more debugging

--- Numerical_projects/Milestone4/python/testing_bessel.py
+++ Numerical_projects/Milestone4/python/testing_bessel.py
@@ -1,26 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('./../../Milestone1/python')
+from plotter_Milestone1 import color_each_regime
+
+plt.style.use('bmh')
 
 data_path = "../../data_testing/"
-# # test of bessel function alone
-# data_bessel = np.loadtxt(data_path+"testing_bessel.txt",skiprows=2).T
-# labels = np.loadtxt(data_path+"testing_bessel.txt",skiprows=1,max_rows=1,dtype=str)
-# x = data_bessel[0]
-# arg = data_bessel[1]
-# j_ell = [data_bessel[i] for i in range(2,len(data_bessel))]
-
-# fig, ax = plt.subplots(2,1,figsize=(12,9),sharex=True)
-# for i in range(len(j_ell)):
-#     if i == 0:
-#         ax[0].plot(x,arg,label="argument to bessel")
-#     if np.all(j_ell[i]==0):
-#         pass
-#     elif i != 2:
-#         pass
-#     else:
-#         ax[1].plot(x,j_ell[i],label=labels[i+2])
-# ax[0].legend(loc=2)
-# ax[1].legend(loc=2)
 
 # Output from Milestone3 with source function
 k_values, _, horizon_entry = np.loadtxt(data_path+"perturbations_k_values.txt",skiprows=1,delimiter="|",unpack=True)
@@ -32,7 +18,6 @@ for k_value in k_values:
         data[q].append(loaded_data[:,i])
 
 x = data["x"][0]
-
 for ik in range(len(k_values)):
     fig, ax = plt.subplots(4,1,figsize=(12,9),sharex=True)
     k = k_values[ik]
@@ -41,6 +26,7 @@ for ik in range(len(k_values)):
     ax[0].plot(x,data["ST"][ik],label="ST")
     ax[0].set_title(r"$\tilde{S}(k,x)$")
     ax[0].legend()
+    color_each_regime(ax[0],x)
 
     for q in range(2,5):
         if np.all(data[quantities[q]][ik]==0):
@@ -49,14 +35,21 @@ for ik in range(len(k_values)):
             ax[1].plot(x,data[quantities[q]][ik]/1e-3,label=quantities[q])
     ax[1].set_title(r"$\tilde{S}(k,x)j_{\ell}[k(\eta_0-\eta(x]/10^{-3}$")
     ax[1].legend(loc=2)
+    color_each_regime(ax[1],x)
     for ind_ell in range(10,len(quantities)):
         if np.all(data[quantities[ind_ell]][ik]==0) or ind_ell>13:
             pass
         else:
             ax[2].plot(x,data[quantities[ind_ell]][ik],label=quantities[ind_ell])
+    ax[2].set_title("Bessel alone")
     ax[2].legend(loc=2)
+    color_each_regime(ax[2],x)
 
     for q in range(6,10):
         ax[3].plot(x,data[quantities[q]][ik],label=quantities[q])
+    ax[3].set_title("Terms in source func")
     ax[3].legend(loc=2)
+    color_each_regime(ax[3],x)
+    
+    fig.savefig('testing_perturbations_k{:.5f}.pdf'.format(k_values[ik]))
 plt.show()
--- Numerical_projects/Milestone4/python/testing_comp.py
+++ Numerical_projects/Milestone4/python/testing_comp.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('./../../Milestone1/python')
+from plotter_Milestone1 import color_each_regime
+
+plt.style.use('bmh')
 
 data_path = "../../data_testing/"
 k_values, _, horizon_entry = np.loadtxt(data_path+"perturbations_k_values.txt",skiprows=1,delimiter="|",unpack=True)
@@ -13,23 +18,24 @@ for k_value in k_values:
 x = data["x"][0]
 
 figs = []
-for ik in range(len(k_values)):
-    fig,ax = plt.subplots()
-    ax.set_title("k: "+str(k_values[ik]))
-    ax.plot(x,data["dPsi"][ik],label="dPsi")
-    ax.plot(x,data["dPhi"][ik],label="dPhi")
-    ax.legend()
-    figs.append(fig)
-
 # for ik in range(len(k_values)):
 #     fig,ax = plt.subplots()
 #     ax.set_title("k: "+str(k_values[ik]))
-#     ax.plot(x,data["term3_1"][ik],label="term3_1")
-#     ax.plot(x,data["term3_2"][ik],label="term3_2")
-#     ax.plot(x,data["term3_3"][ik],label="term3_3")
-    
+#     ax.plot(x,data["dPsi_dx"][ik],label="dPsi_dx")
+#     ax.plot(x,data["dPhi_dx"][ik],label="dPhi_dx")
 #     ax.legend()
+#     color_each_regime(ax,x)
 #     figs.append(fig)
+
+for ik in range(len(k_values)):
+    fig,ax = plt.subplots()
+    ax.set_title("k: "+str(k_values[ik]))
+    ax.plot(x,data["term3_1"][ik],label="term3_1")
+    ax.plot(x,data["term3_2"][ik],label="term3_2")
+    ax.plot(x,data["term3_3"][ik],label="term3_3")
+    ax.legend()
+    color_each_regime(ax,x)
+    figs.append(fig)
 plt.show()
-# for ik,fig in enumerate(figs):
-#     fig.savefig("comp_test_k_{:.5f}.pdf".format(k_values[ik]))
+for ik,fig in enumerate(figs):
+    fig.savefig("comp_test_k_{:.5f}.pdf".format(k_values[ik]))
