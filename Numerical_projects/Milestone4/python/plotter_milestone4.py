03a11e4d4a6db5ffcd4cfd5d05ebc409947fae06
Jakob Borg <jakob.borg@astro.uio.com>
2020-05-20 15:55:37 +0200

    added component power spectrum in plotter

--- Numerical_projects/Milestone4/python/plotter_milestone4.py
+++ Numerical_projects/Milestone4/python/plotter_milestone4.py
@@ -7,38 +7,58 @@ plt.style.use('bmh')
 
 if "toy" in sys.argv[1:]:
     data_path = "../../data_toy/"
-    figname = str(sys.argv[1]) + "_" + str(sys.argv[2])
+    figname_C_ell = str(sys.argv[1]) + "_toy"
+    figname_comp_PS = str(sys.argv[2]) + "_toy"
 else:
     data_path = "../../data/"
-    figname = sys.argv[1]
-filename = sys.argv[1] + ".txt"
+    figname_C_ell = sys.argv[1]
+    figname_comp_PS = sys.argv[2]
+filename_C_ell = sys.argv[1] + ".txt"
+filename_comp_PS = sys.argv[2] + ".txt"
 
-quantities = np.loadtxt(data_path+filename,skiprows=1,max_rows=1,dtype=str)
-data_toy = {}
+# Cell stuff
+quantities = np.loadtxt(data_path+filename_C_ell,skiprows=1,max_rows=1,dtype=str)
 data = {}
-# loaded_data_toy = np.loadtxt(data_path_toy+filename,skiprows=1).T
-loaded_data = np.loadtxt(data_path+filename,skiprows=2).T
+loaded_data = np.loadtxt(data_path+filename_C_ell,skiprows=2).T
 for i,q in enumerate(quantities):
-    # data_toy[q] = loaded_data_toy[i]
     data[q] = loaded_data[i]
-
-# ell = data_toy["ell"]
-# fig, ax = plt.subplots(figsize=(9,12))
-# ax.plot(ell, data_toy["Cell"])
-# ax.set_title("Toy cosmology")
-# ax.set_yscale("log")
-# ax.set_xscale("log")
-# fig.savefig("../figs/CMB_powerspectrum_toy.png")
-# fig.savefig("../figs/CMB_powerspectrum_toy.pdf")
 
 ell = data["ell"]
 fig, ax = plt.subplots(figsize=(9,12))
 for i,q in enumerate(quantities[1:]):
     ax.plot(ell, data[q],label=q)
-ax.set_title("LambdaCDM cosmology")
+ax.set_title("CMB power spectrum")
 ax.set_yscale("log")
 ax.set_xscale("log")
 ax.legend()
-fig.savefig("../figs/" +figname + ".png")
+# fig.savefig("../figs/" +figname_C_ell + ".png")
+
+# Component power spectrum stuff
+k_eq = np.loadtxt(data_path+filename_comp_PS,max_rows=1,usecols=1)
+quantities = np.genfromtxt(data_path+filename_comp_PS,skip_header=1,max_rows=1,dtype=str, delimiter=",",autostrip=True)
+data = {}
+loaded_data = np.loadtxt(data_path+filename_comp_PS,skiprows=2).T
+for i,q in enumerate(quantities):
+    data[q] = loaded_data[i]
+
+k = data[quantities[0]]
+fig, ax = plt.subplots(2,1,figsize=(9,12),sharex=True)
+for i,q in enumerate(quantities[1:]):
+    if q == "radiation":
+        ax[1].plot(k, data[q],label=q)
+    else:
+        ax[0].plot(k, data[q],label=q)
+ax[0].axvline(x=k_eq,label=r"$k_{\rm{eq}} \approx$"+" {:.3f}".format(k_eq))
+fig.suptitle("Component power spectrum")
+ax[1].set_xlabel(r"$k [h/Mpc]$")
+ax[0].set_ylabel(r"$P(k) [Mpc/h]^3$")
+ax[1].set_ylabel(r"$P(k) [Mpc/h]^3$")
+ax[0].set_yscale("log")
+ax[1].set_yscale("log")
+ax[0].set_xscale("log")
+ax[1].set_xscale("log")
+ax[0].legend()
+ax[1].legend()
+# fig.savefig("../figs/" +figname_comp_PS + ".png")
 
 plt.show()
